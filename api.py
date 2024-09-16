bd32473947da60852f63391401690afd0af7c8e8
DmitrySap <dmitrsap@mail.ru>
2024-09-16 21:46:06 +0300

    Update (2)

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from config import BASE_URL, EMAIL
 
 
 def create_bug_data(title, description, step, severity, priority, fixed, assignee):
-    """Создание данных для бага."""
+    """Создание данных для бага"""
     return {
         "title": title,
         "description": description,
@@ -17,7 +17,7 @@ def create_bug_data(title, description, step, severity, priority, fixed, assigne
     }
 
 def create_testcase_data(title, precondition, steps, expected, owner, active):
-    """Создание данных для тест-кейса."""
+    """Создание данных для тест-кейса"""
     return {
         "title": title,
         "precondition": precondition,
@@ -28,48 +28,48 @@ def create_testcase_data(title, precondition, steps, expected, owner, active):
     }
 
 class OkayQA:
-    """Класс для взаимодействия с API OkayQA."""
+    """Класс для взаимодействия с API OkayQA"""
 
     def __init__(self):
-        """Инициализация класса OkayQA и получение токена."""
+        """Инициализация класса OkayQA и получение токена"""
         self.base_url = BASE_URL
         self.my_token = self.get_token()[1]
 
     def request(self, method: str, endpoint: str, json_data=None):
-        """Выполнение HTTP-запроса к API."""
+        """Выполнение HTTP-запроса к API"""
         headers = {'Authorization': f'Bearer {self.my_token}', 'Content-Type': 'application/json'}
         response = requests.request(method, f'{self.base_url}{endpoint}',
                                     headers=headers, json=json_data, timeout=10)
         return response.status_code, response.json()
 
     def get_token(self):
-        """Получение токена для аутентификации."""
+        """Получение токена для аутентификации"""
         response = requests.post(f'{self.base_url}auth/login',
                                  headers={'Content-Type': 'application/json'},
                                  json=EMAIL, timeout=10)
         return response.status_code, response.json().get("access_token")
 
     def get_users(self):
-        """Получение списка пользователей."""
+        """Получение списка пользователей"""
         return self.request('GET', 'users')
 
     def get_all_bugs(self):
-        """Получение всех багов."""
+        """Получение всех багов"""
         return self.request('GET', 'bugs')
 
     def post_create_new_bug(self, title, description, step, severity, priority, fixed, assignee):
-        """Создание нового бага."""
+        """Создание нового бага"""
         bug_data = create_bug_data(title, description, step, severity, priority, fixed, assignee)
         return self.request('POST', 'bugs/create', bug_data)
 
     def put_update_bug(self, last_bug_id, title, description,
                        step, severity, priority, fixed, assignee):
-        """Обновление существующего бага."""
+        """Обновление существующего бага"""
         bug_data = create_bug_data(title, description, step, severity, priority, fixed, assignee)
         return self.request('PUT', f'bugs/{last_bug_id}', bug_data)
 
     def delete_user(self, user_id):
-        """Удаление пользователя по ID."""
+        """Удаление пользователя по ID"""
         return self.request('DELETE', f'users/{user_id}')
 
     def create_new_testcase(self, title, precondition, steps, expected, owner, active):
--- test_api.py
+++ test_api.py
@@ -1,15 +1,18 @@
-from api import OkayQA
+"""Модуль с автотестами"""
 from faker import Faker
+from api import OkayQA
 
 fake = Faker()
 ok = OkayQA()
 
 def test_get_token():
+    """Проверка успешности получения токена"""
     status, token= ok.get_token()
     assert status == 201
     assert token is not None
 
 def test_get_users():
+    """Проверка успешности получения списка пользователей"""
     status, users = ok.get_users()
     assert status == 200
     assert len(users) > 0
@@ -17,11 +20,13 @@ def test_get_users():
     assert user_found['role'] == "admin"
 
 def test_get_all_bugs():
+    """Проверка успешности получения всех багов"""
     status, bugs = ok.get_all_bugs()
     assert status == 200
     assert len(bugs) > 0
 
 def test_post_create_new_bug():
+    """Проверка успешности создания бага"""
     title = "Баг"
     description = "Это тестовый баг"
     step = "Шаг чтобы воспроизвести баг"
@@ -30,12 +35,14 @@ def test_post_create_new_bug():
     fixed = False
     assignee = "Dmitriy"
 
-    status, bug = ok.post_create_new_bug(title, description, step, severity, priority, fixed, assignee)
+    status, bug = ok.post_create_new_bug(title, description, step,
+                                         severity, priority, fixed, assignee)
     assert status == 201
     assert bug['title'] == title, 'Неверный Title бага'
     assert bug['description'] == description, "Неверный Description бага"
 
 def test_put_update_bug():
+    """Проверка успешности обновления бага"""
     bugs = ok.get_all_bugs()[1]
     last_bug_id = bugs[-1]['id']
     title = fake.sentence(nb_words=3)
@@ -46,13 +53,15 @@ def test_put_update_bug():
     fixed = fake.boolean()
     assignee = fake.name()
 
-    status, updated_bug = ok.put_update_bug(last_bug_id, title, description, step, severity, priority, fixed, assignee)
+    status, updated_bug = ok.put_update_bug(last_bug_id, title, description, step,
+                                            severity, priority, fixed, assignee)
     assert status == 200
     assert updated_bug['title'] == title
     assert updated_bug['description'] == description, "Неверный Description бага"
 
 
-def test_delete_user_insufficient_role():
+def test_delete_user():
+    """Проверка успешности удаления пользователя"""
     user_id = 1
     status, response = ok.delete_user(user_id)
     assert status == 403, "Ожидаемый статус 403 Forbidden"
@@ -60,6 +69,7 @@ def test_delete_user_insufficient_role():
     assert response['error'] == "Forbidden", "Ожидаемая ошибка 'Forbidden'"
 
 def test_create_testcase():
+    """Проверка успешности создания тесткейса"""
     title = fake.sentence(nb_words=3)
     precondition = fake.sentence()
     steps = fake.sentence()
@@ -73,16 +83,18 @@ def test_create_testcase():
     assert testcase['precondition'] == precondition, "Неверный Precondition тесткейса"
 
 def test_get_all_testcases():
+    """Проверка успешности получения всех тесткейсов"""
     status, testcases = ok.get_all_testcases()
     assert status == 200
     assert len(testcases['data']) > 0, "Не найдено тесткейсов"
 
 def test_delete_all_testcases():
+    """Проверка успешности удаления всех тесткейсов"""
     status, testcases = ok.get_all_testcases()
     assert status == 200, "Ошибка получения тесткейсов"
     assert len(testcases['data']) > 0, "Не найдено тесткейсов для удаления"
 
     for testcase in testcases['data']:
         testcase_id = testcase['id']
-        delete_status, delete_response = ok.delete_testcases(testcase_id)
+        delete_status = ok.delete_testcases(testcase_id)
         assert delete_status == 200, f"Ошибка при удалении тесткейса с ID {testcase_id}"
