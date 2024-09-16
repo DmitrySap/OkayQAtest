1d0774107cee9e75aa41f279aede42d4b94994e1
DmitrySap <dmitrsap@mail.ru>
2024-09-16 16:04:30 +0300

    Create/delete testcase

--- api.py
+++ api.py
@@ -13,6 +13,15 @@ def create_bug_data(title, description, step, severity, priority, fixed, assigne
         "assignee": assignee
     }
 
+def create_testcase_data(title, precondition, steps, expected, owner, active):
+    return {
+        "title": title,
+        "precondition": precondition,
+        "steps": [steps],
+        "expected": expected,
+        "owner": owner,
+        "active": active,
+    }
 
 class OkayQA:
     def __init__(self):
@@ -43,3 +52,16 @@ class OkayQA:
     def put_update_bug(self, last_bug_id, title, description, step, severity, priority, fixed, assignee):
         bug_data = create_bug_data(title, description, step, severity, priority, fixed, assignee)
         return self.request('PUT', f'bugs/{last_bug_id}', bug_data)
+
+    def delete_user(self, user_id):
+        return self.request('DELETE', f'users/{user_id}')
+
+    def create_new_testcase(self, title, precondition, steps, expected, owner, active):
+        testcase_data = create_testcase_data(title, precondition, steps, expected, owner, active)
+        return self.request("POST", 'test-cases/create', testcase_data)
+
+    def get_all_testcases(self):
+        return self.request('GET', 'test-cases')
+
+    def delete_testcases(self, testcase_id):
+        return self.request('DELETE', f'test-cases/{testcase_id}')
--- test_api.py
+++ test_api.py
@@ -1,5 +1,7 @@
 from api import OkayQA
+from faker import Faker
 
+fake = Faker()
 ok = OkayQA()
 
 def test_get_token():
@@ -36,15 +38,51 @@ def test_post_create_new_bug():
 def test_put_update_bug():
     bugs = ok.get_all_bugs()[1]
     last_bug_id = bugs[-1]['id']
-    title = "Обновленный тайтл"
-    description = "Обновленное описание"
-    step = "Обновленный шаг"
+    title = fake.sentence(nb_words=3)
+    description = fake.sentence()
+    step = fake.sentence()
     severity = "major"
     priority = "medium"
-    fixed = True
-    assignee = "Oleg"
+    fixed = fake.boolean()
+    assignee = fake.name()
 
     status, updated_bug = ok.put_update_bug(last_bug_id, title, description, step, severity, priority, fixed, assignee)
     assert status == 200
     assert updated_bug['title'] == title
     assert updated_bug['description'] == description, "Неверный Description бага"
+
+
+def test_delete_user_insufficient_role():
+    user_id = 1
+    status, response = ok.delete_user(user_id)
+    assert status == 403, "Ожидаемый статус 403 Forbidden"
+    assert response['message'] == "Insufficient role", "Ожидаемое сообщение 'Insufficient role'"
+    assert response['error'] == "Forbidden", "Ожидаемая ошибка 'Forbidden'"
+
+def test_create_testcase():
+    title = fake.sentence(nb_words=3)
+    precondition = fake.sentence()
+    steps = fake.sentence()
+    expected = fake.sentence()
+    owner = fake.name()
+    active = fake.boolean()
+
+    status, testcase = ok.create_new_testcase(title, precondition, steps, expected, owner, active)
+    assert status == 201
+    assert testcase['title'] == title, 'Неверный Title тесткейса'
+    assert testcase['precondition'] == precondition, "Неверный Precondition тесткейса"
+
+def test_get_all_testcases():
+    status, testcases = ok.get_all_testcases()
+    assert status == 200
+    assert len(testcases['data']) > 0, "Не найдено тесткейсов"
+
+def test_delete_all_testcases():
+    status, testcases = ok.get_all_testcases()
+    assert status == 200, "Ошибка получения тесткейсов"
+    assert len(testcases['data']) > 0, "Не найдено тесткейсов для удаления"
+
+    for testcase in testcases['data']:
+        testcase_id = testcase['id']
+        delete_status, delete_response = ok.delete_testcases(testcase_id)
+        assert delete_status == 200, f"Ошибка при удалении тесткейса с ID {testcase_id}"
